01686ced6cd5c3c9b325e7f0995876796b990d24
Shivaot <shivatiwari757@gmail.com>
2020-05-10 16:28:02 +0530

    dispatch actions moved to reducer

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -5,35 +5,14 @@ import CounterOutput from '../../components/CounterOutput/CounterOutput';
 
 
 class Counter extends Component {
-    state = {
-        counter: 0
-    }
-
-    counterChangedHandler = ( action, value ) => {
-        switch ( action ) {
-            case 'inc':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + 1 } } )
-                break;
-            case 'dec':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - 1 } } )
-                break;
-            case 'add':
-                this.setState( ( prevState ) => { return { counter: prevState.counter + value } } )
-                break;
-            case 'sub':
-                this.setState( ( prevState ) => { return { counter: prevState.counter - value } } )
-                break;
-        }
-    }
-
     render () {
         return (
             <div>
                 <CounterOutput value={this.props.ctr} />
-                <CounterControl label="Increment" clicked={() => this.counterChangedHandler( 'inc' )} />
-                <CounterControl label="Decrement" clicked={() => this.counterChangedHandler( 'dec' )}  />
-                <CounterControl label="Add 5" clicked={() => this.counterChangedHandler( 'add', 5 )}  />
-                <CounterControl label="Subtract 5" clicked={() => this.counterChangedHandler( 'sub', 5 )}  />
+                <CounterControl label="Increment" clicked={this.props.onIncrementCounter} />
+                <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
+                <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
+                <CounterControl label="Subtract 5" clicked={this.props.onSubtractCounter}  />
             </div>
         );
     }
@@ -51,6 +30,19 @@ const mapStateToProps = state => {
     };
 }
 
+// we say which type of actions do we want to dispatch inside this container
+// dispatch is again a function which call store.dispatch behind the scenes
+// returns a an object where we can define prop name 
+// which will hold the reference to the functions to be executed
+// whenever onIncrementCounter will be executed () => dispatch() will be executed 
+const mapDispatchToProps = dispatch => {
+    return {
+        onIncrementCounter: () => dispatch({type: 'INCREMENT'}),
+        onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
+        onAddCounter: () => dispatch({type: 'ADD',value: 5}),
+        onSubtractCounter: () => dispatch({type: 'SUBTRACT',value:5})
+    }
+}
 
 /*
 connect is function which takes a function and returns a hoc
@@ -62,7 +54,7 @@ it takes 2 arguments:
 The return of connect() is a wrapper function that takes your component and returns 
 a wrapper component with the additional props it injects.
 */
-export default connect(mapStateToProps)(Counter);
+export default connect(mapStateToProps,mapDispatchToProps)(Counter);
 
 /* 
 after this connect(mapStateToProps) returns a wrapper function
